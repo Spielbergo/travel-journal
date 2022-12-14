d47d26a271a5d2424a5acb8e6a094bde7817eb5d
Scott Sutherland <scott@webcheddar.ca>
2022-12-14 10:25:18 -0500

    Inital commit

---
+++ src/data.js
@@ -0,0 +1,32 @@
+export default trips = [
+    {
+        id: "1",
+        location: "Costa Rica",
+        map: "https://goo.gl/maps/eGszGnuEbM6iefqh6",
+        startDate: "March 06, 2019",
+        endDate: "June 03, 2019",
+        description: "Costa Rica is a rugged, rainforested Central American country with coastlines on the Caribbean and Pacific. Though its capital, San Jose, is home to cultural institutions like the Pre-Columbian Gold Museum, Costa Rica is known for its beaches, volcanoes, and biodiversity. Roughly a quarter of its area is made up of protected jungle, teeming with wildlife including spider monkeys and quetzal birds.",
+        img: "https://deih43ym53wif.cloudfront.net/arenal-volcano-costa-rica-shutterstock_1337924888_ba3a50b8f9.jpeg",
+        visits: "18"
+    },
+    {
+        id: "2",
+        location: "Vancouver",
+        map: "https://goo.gl/maps/YZfAF7Tnm1gVL49q9",
+        startDate: "October 08, 2022, ",
+        endDate: "October 18, 2022",
+        description: "Vancouver, a bustling west coast seaport in British Columbia, is among Canada’s densest, most ethnically diverse cities. A popular filming location, it’s surrounded by mountains, and also has thriving art, theatre and music scenes. Vancouver Art Gallery is known for its works by regional artists, while the Museum of Anthropology houses preeminent First Nations collections.",
+        img: "https://res.cloudinary.com/simpleview/image/upload/v1589990523/clients/vancouverbc/Vancouver_Aerial_2017_1__72115131-4a31-42dc-b369-7a5ccec8273f.jpg",
+        visits: "41"
+    },
+    {
+        id: "2",
+        location: "Nova Scotia",
+        map: "https://goo.gl/maps/99HBLx3MbVWLpona9",
+        startDate: "August 08, 2022, ",
+        endDate: "August 18, 2022",
+        description: "Nova Scotia is one of the thirteen provinces and territories of Canada. It is one of the three Maritime provinces and one of the four Atlantic provinces. Nova Scotia is Latin for 'New Scotland'. Most of the population are native English-speakers, and the province's population is 969,383 according to the 2021 Census.",
+        img: "https://www.novascotia.com/sites/default/files/2022-11/Cape-Forchu-Winter-sunrise-1920x1080.jpg",
+        visits: "9"
+    }
+]
